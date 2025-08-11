4b02ec53375f6293db9e950c20615d6730568dc7
Gabriela Viana Cunha <2411550012@fema.edu.br>
2025-08-11 14:51:50 -0300

    Primeiro commit

---
+++ src/main/java/com/gvcunha/GvcunhaApplication.java
@@ -0,0 +1,13 @@
+package com.gvcunha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GvcunhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GvcunhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gvcunha/GvcunhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvcunha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GvcunhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
