44bc8fd530a8ca671ed89e7b2bb4748ea4e831ce
Gabriela Viana Cunha <2411550012@fema.edu.br>
2025-08-12 10:50:51 -0300

    feat: adicionar a classe de TarefaNaoEncontradaException

---
+++ src/main/java/com/todolist/exception/TarefaNaoEncontradaException.java
@@ -0,0 +1,11 @@
+package com.todolist.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class TarefaNaoEncontradaException extends RuntimeException {
+    public TarefaNaoEncontradaException(String mensagem) {
+        super();
+    }
+}
